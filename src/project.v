200289d68d28d44d62661458a67bd96e33227f71
Kailey White <kailey.white@valpo.edu>
2026-04-27 06:52:47 +0200

    This is hopefully it

--- src/project.v
+++ src/project.v
@@ -17,20 +17,13 @@ module tt_um_kaileywhite (
 );
 
 
-    wire [3:0] a = ui_in[3:0];
-    wire [3:0] b = un_in[7:4];
-
-    wire [7:0] product;
-
-    assign product = a * b
-
-
+   
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = product;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 8'b0;
-  assign uio_oe  = 8'b0;
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, uio_in};
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
