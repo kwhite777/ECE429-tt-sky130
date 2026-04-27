11b08be8b04f2aa77d89b9ac84c1d6dd935b20e5
Kailey White <kailey.white@valpo.edu>
2026-04-27 05:09:07 +0200

    fix docs again

--- test/tb.v
+++ test/tb.v
@@ -4,7 +4,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module pm32 ();
 
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
