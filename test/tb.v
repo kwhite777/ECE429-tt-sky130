0dca5bd416e4a3254ddb66d92d6e601cff388f81
Kailey White <kailey.white@valpo.edu>
2026-04-27 06:01:43 +0200

    IDK

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  pm32  user_project (
+  pm32 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
